3cad2e6d932f6d9144ad52ee0ea796d6d7c6c945
Luca Candela <ilcande_prime@live.it>
2022-01-04 22:12:16 +0100

    add songs and song components. move sign out to top right user tab

--- components/Center.js
+++ components/Center.js
@@ -1,10 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { shuffle } from "lodash";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
+import Songs from "../components/Songs";
 
 const colors = [
   "from-indigo-500",
@@ -41,20 +42,20 @@ function Center() {
   console.log(playlist);
 
   return (
-    <div className="flex-grow">
+    <div className="flex-grow h-screen overflow-y-scroll">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+        <div onClick={() => signOut()} className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
             alt=""
           />
-          <h2>{session?.user.name}</h2>
-          <ChevronDownIcon className="h-5 w-5"/>
+          <h2 className="text-white">{session?.user.name}</h2>
+          <ChevronDownIcon className="h-5 w-5 text-white"/>
         </div>
       </header>
 
-      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
         <img
           className="h-44 w-44 shadow-2xl"
           src={playlist?.images?.[0]?.url}
@@ -62,6 +63,10 @@ function Center() {
         />
         <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
       </section>
+
+      <div>
+        <Songs />
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { getSession } from "next-auth/react";
 import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 
@@ -15,3 +16,13 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  }
+}
+
